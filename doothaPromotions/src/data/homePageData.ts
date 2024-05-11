dae96463111b810423a00d0c48c1730a45967113
shaikafrid143143 <70587992+shaikafrid143143@users.noreply.github.com>
2024-05-11 05:03:04 +0000

    Pending changes exported from your codespace

---
+++ doothaPromotions/src/data/homePageData.ts
@@ -0,0 +1,19 @@
+import { carouselViewPropsDataType } from "../types/homePageDataTypes";
+
+export const homeMainCarouselViewData: carouselViewPropsDataType = [
+  {
+    image: "homeMain1.jpg",
+  },
+  {
+    image: "homeMain2.jpg",
+  },
+  {
+    image: "homeMain3.jpg",
+  },
+  {
+    image: "homeMain4.jpg",
+  },
+  {
+    image: "homeMain5.jpg",
+  },
+];
--- doothaPromotions/src/ui/CarouselView.tsx
+++ doothaPromotions/src/ui/CarouselView.tsx
@@ -2,19 +2,34 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,CarouselPrevious
 } from "../../@/components/ui/carousel";
+import { carouselViewPropsDataType } from "../types/homePageDataTypes";
 interface CarouselViewInterfaceInterface {
-  dataArray: any[];
+  dataArray: carouselViewPropsDataType;
+  autoPlay?:boolean,
+  dots?:boolean,
+  navigation?:boolean
 }
 function CarouselView({ dataArray }: CarouselViewInterfaceInterface) {
   return (
     <div>
-      <Carousel>
-        <CarouselContent>
+      <Carousel className="w-full" 
+      plugins={[
+        
+      ]}>
+        <CarouselContent className="w-[100vw]">
           {dataArray?.map((item: any, index) => {
-            return <CarouselItem key={index}></CarouselItem>;
+            return <CarouselItem key={index} className="">
+              <div className="flex aspect-square items-center justify-center">
+              <img src={item?.image} className="object-fill w-[100vw] h-[30vh]"/>
+              </div>
+            </CarouselItem>;
           })}
         </CarouselContent>
+        
+        {/* <CarouselPrevious/>
+        <CarouselNext/> */}
       </Carousel>
     </div>
   );
